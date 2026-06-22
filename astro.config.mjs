e44c358a90e9cb1259e01309776189b1e7a2f5f3
Palak1221-dev <palakshekhawat12@gmail.com>
2026-06-22 11:48:46 +0530

    Add support for deployment under /word-counter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tooltails.com',
+  base: '/word-counter',
   integrations: [sitemap()],
   devToolbar: {
     enabled: false,
